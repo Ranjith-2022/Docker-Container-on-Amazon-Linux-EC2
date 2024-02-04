7b7aeb20e42ad3652127d0f5166c16fcd0e87f90
173823220-myseneca <rmanian@myseneca.ca>
2024-02-04 18:31:49 +0000

    Modified terraform structure

--- terraform/instances/config.tf
+++
@@ -1,7 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "clo835-assignment1-ranjith"               
-#     key    = "ec2/terraform.tfstate" 
-#     region = "us-east-1"                        
-#   }
-# }
